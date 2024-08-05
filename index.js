311c81495443ad839c858ac2353404e58a8fcca0
Samnejad92 <m.samnejad92@gmail.com>
2024-08-05 22:00:06 +0330

    bug fixed

--- index.js
+++ index.js
@@ -42,11 +42,11 @@ gridButton.addEventListener("click",()=>{
     let count=0
     for (let i = 0; i < gridHeight.value; i++) {
         count +=2
-        let div=document.createEvent("div")
+        let div=document.createElement("div")
         div.classList.add("gridRow")
         for (let j = 0; j < gridWidth.value; j++) {
             count+=2
-            let col=document.createEvent("div")
+            let col=document.createElement("div")
             col.classList.add("gridCol")
             col.setAttribute("id",`gridCol${count}`)
             col.addEventListener(events[deviceType].down,()=>{
@@ -63,7 +63,7 @@ gridButton.addEventListener("click",()=>{
                     clientX,
                     !isTouchDevice()?e.clientY:e.touches[0].
                     clientY,
-                ),id
+                ).id
                 checker(elementId)
             })
             col.addEventListener(events[deviceType].up,()=>{
